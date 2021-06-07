e67ce6970d97dc4ff762d0ca5d8aa25c4b3e541c
Paul Blackwell <blackwellpaul34@gmail.com>
2021-06-07 23:24:11 +0100

    Started work on the container

--- demo-website/src/App.js
+++ demo-website/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Container from "./components/Container";
 
 function App() {
   return (
     <div className="App">
-      <h1>I am a test</h1>
+      <Container>
+        <h1 className="text-blueGray-200 text-4xl font-extrabold">Cocktail finder</h1>
+        <p className="">Find your perfect cocktail today! Just click on the “Generate a cocktail” button to generate a random cocktail. </p>
+      </Container>
     </div>
   );
 }
